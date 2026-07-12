40cdb0407707ab4bb8ecafb79c840a070cf70fde
Vivek Choudhary <44796815+choudharyvivek9719@users.noreply.github.com>
2026-07-12 21:23:23 +0530

    Add API service using axios for HTTP requests

---
+++ frontend/src/services/api.js
@@ -0,0 +1,20 @@
+// src/services/api.js
+
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: "http://localhost:8000",
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
+
+api.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        console.error("API Error:", error);
+        return Promise.reject(error);
+    }
+);
+
+export default api;
